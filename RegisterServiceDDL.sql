53291499b3979cfa4594ccead2925bba510a10a1
killingrocks <niclaus.a@gmail.com>
2017-06-12 11:25:04 -0500

    uuid(0,0)

--- RegisterServiceDDL.sql
+++ RegisterServiceDDL.sql
@@ -3,7 +3,9 @@ CREATE EXTENSION "uuid-ossp";
 CREATE TABLE product (
   id uuid NOT NULL,
   lookupcode character varying(32) NOT NULL DEFAULT(''),
-  count int NOT NULL DEFAULT(0),
+  price int NOT NULL DEFAULT(0),
+  quantity int NOT NULL DEFAULT(0),
+  active boolean NOT NULL DEFAULT(FALSE),
   createdon timestamp without time zone NOT NULL DEFAULT now(),
   CONSTRAINT product_pkey PRIMARY KEY (id)
 ) WITH (
@@ -14,25 +16,22 @@ CREATE INDEX ix_product_lookupcode
   ON product
   USING btree
   (lower(lookupcode::text) COLLATE pg_catalog."default");
-
+  
 INSERT INTO product VALUES (
        uuid_generate_v4()
      , 'lookupcode1'
+     , 50 
+     , 10
+     , true
+     , current_timestamp
+);
+
+INSERT INTO product VALUES (
+       uuid_generate_v4()
+     , 'lookupcode2'
      , 100
-     , current_timestamp
-);
-
-INSERT INTO product VALUES (
-       uuid_generate_v4()
-     , 'lookupcode1'
-     , 125
-     , current_timestamp
-);
-
-INSERT INTO product VALUES (
-       uuid_generate_v4()
-     , 'lookupcode3'
-     , 150
+     , 2
+     , false
      , current_timestamp
 );
 
@@ -55,3 +54,49 @@ CREATE INDEX ix_employee_employeeid
   ON employee
   USING hash(employeeid);
 
+
+INSERT INTO employee VALUES (
+       uuid_generate_v4()
+     , 'rystewar'
+     , 'Ryan'
+     , 'Stewart'
+     , 'wait'
+     , false
+     , 3
+     , uuid_generate_v4()
+     , current_timestamp
+);
+
+CREATE TABLE transactionentity (
+ id uuid NOT NULL,
+  lookupcode character varying(32) NOT NULL DEFAULT(''),
+  price int NOT NULL DEFAULT(0),
+  quantity int NOT NULL DEFAULT(0),
+   CONSTRAINT record_transactionentity PRIMARY KEY (id)
+) WITH (
+  OIDS=FALSE
+);
+
+
+CREATE TABLE transaction (
+  id uuid NOT NULL,
+  cashierid character varying(32) NOT NULL DEFAULT(''),
+  totalamount int NOT NULL DEFAULT(0),
+  transactiontype int NOT NULL DEFAULT(0),
+  referenceid uuid NOT NULL,
+  CONSTRAINT reference_cashieried FOREIGN KEY(referenceid)
+	REFERENCES transaction (id) MATCH SIMPLE
+	ON UPDATE NO ACTION ON DELETE NO ACTION,
+  createdon timestamp without time zone NOT NULL DEFAULT now(),
+  CONSTRAINT record_transaction PRIMARY KEY (id)
+) WITH (
+  OIDS=FALSE
+);
+INSERT INTO transaction VALUES (
+       '00000000-0000-0000-0000-000000000000'
+     , 'same'
+     , '100'
+     , '0'
+     , '00000000-0000-0000-0000-000000000000'
+	 , current_timestamp
+);
